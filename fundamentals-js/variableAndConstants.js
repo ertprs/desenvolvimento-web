e5173e0d06acb0873c6176ba0b095753a79feedb
renanmoura6941 <naneraruom@gmail.com>
2020-10-24 18:47:56 -0300

    basic

---
+++ fundamentals-js/variableAndConstants.js
@@ -0,0 +1,6 @@
+var a = 3; //variaveis do tipo var pode ser redeclaradas
+let b = 4;
+
+const PI = 3.1415;
+
+console.log(a,b,PI);
---
+++ fundamentals-js/weakTyping.js
@@ -0,0 +1,7 @@
+let weakTyping = "any";
+console.log(typeof weakTyping);
+
+weakTyping = 3.1516;
+console.log(typeof weakTyping);
+
+//tipagem fraca, uma variável pode assumir vários tipos de dados
