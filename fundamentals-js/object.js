3ef9b849cd96c8df0d2880d0ffb223f25716481a
renanmoura6941 <naneraruom@gmail.com>
2020-10-28 11:34:20 -0300

    seção:3, aula:24 object

---
+++ fundamentals-js/object.js
@@ -0,0 +1,17 @@
+const myObject = {}
+
+myObject.name = 'Russo'
+myObject.age = 24
+myObject['height'] = 1.6;
+
+console.log('object in JS:')
+console.log(myObject)
+
+const myObjectTwo = {
+    name:'loop',
+    age:5,
+    height:1
+}
+
+console.log('object two in JS:')
+console.log(myObjectTwo)
